ccc6a57ba639badf01f6bbcdc595ff8c80f0dd8d
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-09-03 17:48:53 +0800

    feat: add demo03

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "./App.css";
 import { Demo01 } from "./examples/Demo01";
 import { Demo02 } from "./examples/Demo02";
+import { Demo03 } from "./examples/Demo03";
 
 const App = () => {
   return (
     <>
       {/* <Demo01 /> */}
-      <Demo02 />
+      {/* <Demo02 /> */}
+      <Demo03 />
     </>
   );
 };
